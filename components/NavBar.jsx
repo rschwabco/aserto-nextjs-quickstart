ff302a1c0cda18509f6e6eb0849e053088e9e397
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-11-23 15:48:08 -0800

    Reset

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,14 +8,9 @@ import {
     NavItem,
     NavLink
 } from 'reactstrap'
-import { useUser } from '@auth0/nextjs-auth0';
-import AnchorLink from './AnchorLink';
-import Image from 'next/image'
 import styles from '../styles/NavBar.module.css'
 
-
 const NavBar = () => {
-    const { user, isLoading } = useUser();
     return (
         <div>
           <Navbar
@@ -41,51 +36,10 @@ const NavBar = () => {
                   </NavLink>
                 </NavItem>
               </Nav>
-              {!isLoading && !user && (
-                <Nav>
-                    <NavItem id="qsLoginBtn">
-                      <AnchorLink
-                        href="/api/auth/login"
-                        className="btn btn-primary btn-margin"
-                        tabIndex={0}
-                        testId="navbar-login-desktop">
-                        Log in
-                      </AnchorLink>
-                    </NavItem>
-                </Nav>
-              )}
-              {user && (
-                <Nav>
-                    <NavItem className={styles.userimage}>
-                        <Image
-                          src={user.picture}
-                          alt="Profile"
-                          className="nav-user-profile d-inline-block rounded-circle mr-3"
-                          width="50"
-                          height="50"
-                          decode="async"
-                          data-testid="navbar-picture-mobile"
-                        />
-                    </NavItem>
-                    <NavItem className={styles.username}>
-                      <span className="d-inline-block" data-testid="navbar-user-mobile">
-                          {user.name}
-                        </span>
-                    </NavItem>
-                    <NavItem id="qsLogoutBtn">
-                      <AnchorLink
-                        href="/api/auth/logout"
-                        className="btn btn-secondary btn-margin"
-                        testId="navbar-logout-mobile">
-                        Log out
-                      </AnchorLink>
-                    </NavItem>
-                </Nav>
-              )}
+              {/* User navbar items to be added here */}
             </Collapse>
           </Navbar>
         </div>
-
     )
 }
 
--- pages/protectedPage.jsx
+++ pages/protectedPage.jsx
@@ -1,44 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { Button } from 'reactstrap';
 import styles from '../styles/ProtectedPage.module.css'
 
-
-export default withPageAuthRequired(function ProtectedPage(){
-    const [state, setState] = useState({ response: undefined, error: undefined, displayState: undefined });
-
-    const fetchApi = async () => {
-        try {
-          const response = await fetch('/api/protected');
-          const { status } = response
-          const data = await response.json();
-
-          setState(previous => ({ ...previous, response: data, error: undefined, status }))
-        } catch (error) {
-          setState(previous => ({ ...previous, response: undefined, error }))
-        }
-    };
-
-    useEffect(() => {
-        const getDisplayState = async () => {
-            try {
-              const response = await fetch('/api/displayState',
-                {   method: 'POST',
-                    body: JSON.stringify({ path: '/api/protected', method: 'GET'})
-                }
-              );
-              const data = await response.json();
-              setState(previous => ({ ...previous, displayState: data, error: undefined }))
-            } catch (error) {
-              setState(previous => ({ ...previous, displayState: undefined, error }))
-            }
-        }
-
-        getDisplayState()
-    }, [])
-
-    const { response, displayState, status, error } = state
-
+export default function ProtectedPage(){
     return (
         <div className={styles.main}>
             <div className={styles.topMain}>
@@ -46,57 +9,24 @@ export default withPageAuthRequired(function ProtectedPage(){
                     <h1>Protected Page</h1>
                 </div>
                 <div>
-                    <Button className="mt-1"
-                        onClick={e => {
-                            e.preventDefault()
-                            fetchApi()
-                        }}
-                        data-testid="external-action"
-                        color={displayState?.enabled ? 'primary' : 'secondary'}
-                        disabled={!displayState?.enabled}
-                    >
-                      Fetch protected resource
-                    </Button>
+                    {/* Button to be added here */}
                 </div>
 
             </div>
             <div className={styles.centerMain}>
                 <div className={styles.welcomeMessage}>
-                    {displayState?.visible && <h6>This message is visible only to Executives</h6>}
+                    {/* `visible` message to be added here */}
                 </div>
 
                 <div className="mt-3">
-                    {status !== 500 && status !== 403 && response?.msg &&
-                        <div>
-                            <div className={styles.lottie}></div>
-                            <div>
-                                {response.msg}
-                            </div>
-                        </div>
-                    }
-                    {status === 403 &&
-                        <div>
-                            <div className={styles.sadLottie}></div>
-                            <div>
-                                Access is forbidden for this user.
-                            </div>
-                        </div>
-                    }
-                    {status === 500 &&
-                        <div>
-                            <div className={styles.sadLottie}></div>
-                            <div>
-                                An error has occurred.
-                            </div>
-                        </div>
-                    }
+                   {/* Access messages to be added here */}
+                </div>
+
+                <div className="mt-3">
+                   {/* Error message to be added here */}
                 </div>
             </div>
-            {error && (
-                <div className="mt-3">
-                    {error}
-                </div>
-            )}
+
         </div>
     )
-})
+}
